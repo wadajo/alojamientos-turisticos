9386dd5effc299dd267ca392caba57f8f30d055f
jestravis <jestravis@paradigmadigital.com>
2023-08-23 20:35:44 +0200

    Connection string parametrizada en properties

--- src/main/java/com/wadajo/turismomadrid/infrastructure/configuration/DbConfiguration.java
+++ src/main/java/com/wadajo/turismomadrid/infrastructure/configuration/DbConfiguration.java
@@ -1,6 +1,7 @@
 package com.wadajo.turismomadrid.infrastructure.configuration;
 
 import com.mongodb.ConnectionString;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.mongo.MongoConnectionDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +9,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration(proxyBeanMethods = false)
 public class DbConfiguration {
 
+    @Value("${turismomadrid.mongodb.local}")
+    private String connectionString;
+
     @Bean
     MongoConnectionDetails myMongoConnectionDetails(){
-         return () -> new ConnectionString("mongodb://leon:pelonchi@localhost:27017/turismo?ssl=false");
+         return () -> new ConnectionString(connectionString);
     }
 }
