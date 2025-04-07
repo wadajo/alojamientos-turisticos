bd3ec4d47eb14ca42259949bce038619965a9332
Julio César <juliocesareb@hotmail.com>
2025-04-07 12:48:05 +0200

    Refactor de tests y añadido test de actualizarDb

--- src/test/java/com/wadajo/turismomadrid/domain/service/TurismoServiceTest.java
+++ src/test/java/com/wadajo/turismomadrid/domain/service/TurismoServiceTest.java
@@ -3,15 +3,18 @@ package com.wadajo.turismomadrid.domain.service;
 import com.wadajo.turismomadrid.application.client.AlojamientosClient;
 import com.wadajo.turismomadrid.application.exception.ResponseTypeDtoException;
 import com.wadajo.turismomadrid.application.repository.*;
+import com.wadajo.turismomadrid.domain.document.HotelDocument;
 import com.wadajo.turismomadrid.domain.dto.cmadrid.AlojamientoTuristicoRaw;
 import com.wadajo.turismomadrid.domain.dto.cmadrid.AlojamientosTuristicosResponseDto;
 import com.wadajo.turismomadrid.domain.dto.cmadrid.enums.TipoAlojamiento;
 import com.wadajo.turismomadrid.domain.model.AlojamientoTuristico;
+import com.wadajo.turismomadrid.util.TestConstants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
@@ -22,6 +25,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -76,10 +80,18 @@ class TurismoServiceTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        when(conversionService.convert(Mockito.any(AlojamientoTuristicoRaw.class), eq(AlojamientoTuristico.class)))
+            .thenAnswer(invocation -> {
+                AlojamientoTuristicoRaw raw = invocation.getArgument(0);
+                return new AlojamientoTuristico.Hotel(
+                    raw.via_tipo(), raw.via_nombre(), raw.numero(), raw.portal(), raw.bloque(), raw.planta(), raw.puerta(), raw.escalera(), raw.denominacion(), raw.cdpostal(), raw.localidad(), TipoAlojamiento.valueOf(raw.alojamiento_tipo()));
+            });
+        when(conversionService.convert(Mockito.any(AlojamientoTuristico.Hotel.class), eq(HotelDocument.class)))
+            .thenReturn(new HotelDocument());
     }
 
     @Test
-    void debeBorrarTodo() {
+    void debeBorrarTodo(CapturedOutput output) {
         turismoService.borrarTodo();
 
         verify(apartamentoRuralMongoRepository).deleteAll();
@@ -94,10 +106,12 @@ class TurismoServiceTest {
         verify(hotelRuralMongoRepository).deleteAll();
         verify(pensionMongoRepository).deleteAll();
         verify(viviendaTuristicaMongoRepository).deleteAll();
+
+        assertThat(output).contains("Borradas todas las colecciones");
     }
 
     @Test
-    void debeObtenerLosAlojamientosTuristicos() throws ResponseTypeDtoException {
+    void debeObtenerLosAlojamientosTuristicos(CapturedOutput output) throws ResponseTypeDtoException {
         AlojamientoTuristicoRaw raw1 = new AlojamientoTuristicoRaw("","2","HOTEL","3-HOTEL","","","HM-127","del Pez","HOTEL DEL TEST","CALLE","28012","Madrid","","");
         AlojamientoTuristicoRaw raw2 = new AlojamientoTuristicoRaw("","5","HOTEL","3-HOTEL","","","HM-123","del Pez","HOTEL DEL ENSAYO","CALLE","28012","Madrid","","");
         List<AlojamientoTuristicoRaw> rawList = new ArrayList<>();
@@ -108,24 +122,21 @@ class TurismoServiceTest {
         when(alojamientosClient.getResponseRaw())
                 .thenReturn(responseDto);
 
-        when(conversionService.convert(raw1, AlojamientoTuristico.class))
-                .thenReturn(new AlojamientoTuristico.Hotel("Calle", "del Pez", "2", "", "", "", "", "", "HOTEL DEL TEST", "28012", "Madrid", TipoAlojamiento.HOTEL));
-        when(conversionService.convert(raw2, AlojamientoTuristico.class))
-                .thenReturn(new AlojamientoTuristico.Hotel("Calle", "del Pez", "5", "", "", "", "", "", "HOTEL DEL ENSAYO", "28012", "Madrid", TipoAlojamiento.HOTEL));
-
         List<AlojamientoTuristico> result = turismoService.getAlojamientosTuristicos();
 
         assertThat(result).size().isEqualTo(2);
+        assertThat(output).contains(TestConstants.RESULTADO_OUTPUT_MOCKS);
     }
 
     @Test
-    void debeDevolverUnaListaVaciaCuandoEnElServidorNoHayAlojamientos() {
+    void debeDevolverUnaListaVaciaCuandoEnElServidorNoHayAlojamientos(CapturedOutput output) {
         when(alojamientosClient.getResponseRaw())
             .thenReturn(new AlojamientosTuristicosResponseDto(null));
 
         List<AlojamientoTuristico> result = turismoService.getAlojamientosTuristicos();
 
         assertThat(result).isEmpty();
+        assertThat(output.getOut()).contains("Resultado: Total alojamientos turisticos: 0");
     }
 
     @Test
@@ -141,6 +152,26 @@ class TurismoServiceTest {
 
         assertThat(result).isEmpty();
         assertThat(output.getOut()).contains("not recognized alojamiento tipo: BIZARRO");
+    }
+
+    @Test
+    void debeActualizarAlojamientoEnDbEInformarElResultado(CapturedOutput output) {
+        AlojamientoTuristicoRaw raw1 = new AlojamientoTuristicoRaw("","2","HOTEL","3-HOTEL","","","HM-127","del Pez","HOTEL DEL TEST","CALLE","28012","Madrid","","");
+        AlojamientoTuristicoRaw raw2 = new AlojamientoTuristicoRaw("","5","HOTEL","3-HOTEL","","","HM-123","del Pez","HOTEL DEL ENSAYO","CALLE","28012","Madrid","","");
+        List<AlojamientoTuristicoRaw> rawList = new ArrayList<>();
+        rawList.add(raw1);
+        rawList.add(raw2);
+        AlojamientosTuristicosResponseDto responseDto = new AlojamientosTuristicosResponseDto(rawList);
+
+        when(alojamientosClient.getResponseRaw())
+            .thenReturn(responseDto);
+
+        var result = turismoService.actualizarAlojamientosEnDb();
+
+        assertThat(result).isEqualTo("Han sido actualizados en DB: 2 alojamientos.");
+        assertThat(output)
+            .contains(TestConstants.RESULTADO_OUTPUT_MOCKS)
+            .contains("Guardados en DB 2 hoteles.");
     }
 
 }
--- src/test/java/com/wadajo/turismomadrid/util/TestConstants.java
+++ src/test/java/com/wadajo/turismomadrid/util/TestConstants.java
@@ -1,6 +1,7 @@
 package com.wadajo.turismomadrid.util;
 
 public class TestConstants {
+    public static final String GRAPHQL = "/graphql";
     public static final String ALOJAMIENTOS_QUERY_JSON_FILE = "src/test/resources/request/queryAlojamientos.json";
     public static final String ALOJAMIENTOS_QUERY_TIPO_JSON_FILE = "src/test/resources/request/queryAlojamientos-tipo.json";
     public static final String ALOJAMIENTOS_QUERY_TIPO_VACIO_JSON_FILE = "src/test/resources/request/queryAlojamientos-tipo-vacio.json";
@@ -12,8 +13,10 @@ public class TestConstants {
 
     public static final String RESULTADO_BASE_OUT_QUERY = "Resultado: Total alojamientos turisticos: 12. {CASA_RURAL=1, HOTEL_APART=1, APARTAMENTO_RURAL=1, CASA_HUESPEDES=1, HOTEL_RURAL=1, PENSION=1, HOSTERIAS=1, CAMPING=1, HOSTAL=1, VIVIENDAS_TURISTICAS=1, APART_TURISTICO=1, HOTEL=1}";
     public static final String RESULTADO_FILTRADO_OUT_QUERY = "Resultado: Total alojamientos turisticos: 1. {CASA_RURAL=0, HOTEL_APART=0, APARTAMENTO_RURAL=0, CASA_HUESPEDES=0, HOTEL_RURAL=0, PENSION=0, HOSTERIAS=0, CAMPING=0, HOSTAL=0, VIVIENDAS_TURISTICAS=0, APART_TURISTICO=1, HOTEL=0}";
+    public static final String RESULTADO_OUTPUT_MOCKS = "Resultado: Total alojamientos turisticos: 2. {CASA_RURAL=0, HOTEL_APART=0, APARTAMENTO_RURAL=0, CASA_HUESPEDES=0, HOTEL_RURAL=0, PENSION=0, HOSTERIAS=0, CAMPING=0, HOSTAL=0, VIVIENDAS_TURISTICAS=0, APART_TURISTICO=0, HOTEL=2}";
+
     public static final String RESULTADO_API_ACTUALIZARDB = "Han sido actualizados en DB: 12 alojamientos.";
+
     public static final String RESULTADO_API_BORRAR = "Borrados";
     public static final String RESULTADO_OUT_BORRAR = "Borradas todas las colecciones";
-    public static final String GRAPHQL = "/graphql";
 }
