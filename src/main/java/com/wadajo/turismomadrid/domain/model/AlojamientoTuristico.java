6e727497d1c249c93605f40d40cc9e61a5286f21
Julio César <juliocesareb@hotmail.com>
2025-06-20 13:42:08 +0200

    Los records de AlojamientoTuristico no pueden ser nulos

--- src/main/java/com/wadajo/turismomadrid/domain/model/AlojamientoTuristico.java
+++ src/main/java/com/wadajo/turismomadrid/domain/model/AlojamientoTuristico.java
@@ -1,7 +1,9 @@
 package com.wadajo.turismomadrid.domain.model;
 
 import com.wadajo.turismomadrid.domain.dto.cmadrid.enums.TipoAlojamiento;
+import org.jspecify.annotations.NullMarked;
 
+@NullMarked
 public sealed interface AlojamientoTuristico {
     
     record ApartamentoRural(String via_tipo, String via_nombre, String numero, String portal, String bloque, String planta, String puerta, String signatura, String categoria, String escalera, String denominacion, String cdpostal, String localidad,TipoAlojamiento alojamiento_tipo) implements AlojamientoTuristico { }
